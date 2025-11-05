003a1b57d81635d365a9e954e3c637561e4b6d1b
Daniel Johansson <danieltagegustav@gmail.com>
2025-11-05 14:29:36 +0100

    helper now connects to database, and storage is kept

--- dbhelper/main.go
+++ dbhelper/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -43,8 +44,28 @@ func main() {
 	coll := client.Database("spellwrong").Collection("misspellings")
 
 	count, err := coll.CountDocuments(context.TODO(), bson.D{})
-	if err != nil || count == 0 {
+	if err != nil {
+		panic("Could not read from database")
+	}
+	if count == 0 {
 		//collection does not exist. Create it
+		file, err := os.Open("./misspellings.json")
+		if err != nil {
+			panic("Failed to open JSON file")
+		}
+		defer file.Close()
+		var misspellings map[string][]string
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&misspellings)
+		if err != nil {
+			panic("Could not decode JSON file")
+		}
+		fmt.Println(misspellings["about"])
+
+		_, err = coll.InsertOne(context.TODO(), bson.M{"data": misspellings})
+		if err != nil {
+			panic("Failed to populate database")
+		}
 		fmt.Println("Created database with misspellings")
 	}
 	fmt.Println("Database ready.")
